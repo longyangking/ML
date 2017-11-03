8def611632e983e9e97b37fd63bbd27b58a121fa
Yang Long <longyang_123@yeah.net>
2017-11-03 21:54:13 +0800

    add examples

--- sklearn/dataset_nn.py
+++ sklearn/dataset_nn.py
@@ -5,12 +5,15 @@ from sklearn.metrics import roc_curve, precision_recall_curve,classification_rep
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import label_binarize
 from sklearn.multiclass import OneVsRestClassifier
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.neural_network import MLPClassifier
 from sklearn.datasets import make_circles,make_moons,make_friedman1,make_s_curve,make_gaussian_quantiles,make_swiss_roll
+
 
 n_samples = 400
 noise = 0.1
 distance = 1.0
+
+classifier = MLPClassifier(solver='lbfgs', alpha=1e-5,hidden_layer_sizes=(20, 10), random_state=1)
 
 # Normal Dataset
 X = np.zeros((2*n_samples,2))
@@ -22,7 +25,7 @@ X[n_samples:,1] = np.random.normal(loc=distance/2, scale=0.5, size=[n_samples])
 y[n_samples:] = np.ones(n_samples)
 
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2)
-classifier = DecisionTreeClassifier()
+classifier = MLPClassifier()
 classifier.fit(X_train,y_train)
 y_pred = classifier.predict(X_test)
 plt.figure()
@@ -44,7 +47,6 @@ plt.ylabel('$x_2$')
 # Circle Dataset
 [X,y] = make_circles(n_samples=n_samples, noise=noise, factor=0.5)
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2)
-classifier = DecisionTreeClassifier()
 classifier.fit(X_train,y_train)
 y_pred = classifier.predict(X_test)
 plt.figure()
@@ -66,7 +68,6 @@ plt.ylabel('$x_2$')
 # Circle Dataset
 [X,y] = make_moons(n_samples=n_samples, noise=noise)
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2)
-classifier = DecisionTreeClassifier()
 classifier.fit(X_train,y_train)
 y_pred = classifier.predict(X_test)
 plt.figure()
